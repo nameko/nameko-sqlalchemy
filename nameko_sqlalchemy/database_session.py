5ad3146d64439baec17edf956216692700c6f75a
scarchik <scarchik@gmail.com>
2017-09-19 12:05:37 +0300

    - Add availability to specify sqlalchemy session options - Add availability to specify db engine options

--- nameko_sqlalchemy/database_session.py
+++ nameko_sqlalchemy/database_session.py
@@ -4,33 +4,39 @@ from nameko.extensions import DependencyProvider
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+SQL_ALCHEMY_KEY = 'SQLALCHEMY'
 DB_URIS_KEY = 'DB_URIS'
+ENGINE_OPTIONS_KEY = 'ENGINE_OPTIONS'
 
 
 class DatabaseSession(DependencyProvider):
-    def __init__(self, declarative_base):
+    def __init__(self, declarative_base, **session_options):
         self.declarative_base = declarative_base
         self.sessions = WeakKeyDictionary()
+        self.session_options = session_options
+
+        self.db_uri = None
+        self.engine = None
 
     def setup(self):
         service_name = self.container.service_name
-        decl_base_name = self.declarative_base.__name__
-        uri_key = '{}:{}'.format(service_name, decl_base_name)
+        declarative_base_name = self.declarative_base.__name__
+        uri_key = '{}:{}'.format(service_name, declarative_base_name)
+        config = self.container.config
 
-        db_uris = self.container.config[DB_URIS_KEY]
+        db_uris = config[SQL_ALCHEMY_KEY][DB_URIS_KEY] if SQL_ALCHEMY_KEY in config else config[DB_URIS_KEY]
         self.db_uri = db_uris[uri_key].format({
             'service_name': service_name,
-            'declarative_base_name': decl_base_name,
+            'declarative_base_name': declarative_base_name,
         })
-        self.engine = create_engine(self.db_uri)
+        self.engine = create_engine(self.db_uri, config.get(SQL_ALCHEMY_KEY, {}).get(ENGINE_OPTIONS_KEY, {}))
 
     def stop(self):
         self.engine.dispose()
         del self.engine
 
     def get_dependency(self, worker_ctx):
-
-        session_cls = sessionmaker(bind=self.engine)
+        session_cls = sessionmaker(bind=self.engine, **self.session_options)
         session = session_cls()
 
         self.sessions[worker_ctx] = session
@@ -39,6 +45,7 @@ class DatabaseSession(DependencyProvider):
     def worker_teardown(self, worker_ctx):
         session = self.sessions.pop(worker_ctx)
         session.close()
+
 
 # backwards compat
 Session = DatabaseSession
