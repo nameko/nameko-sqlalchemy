e2addaca9702b782edd59f351130a8190633c645
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-10-09 16:52:03 +0100

    Add a working draft of a dependency provider with session scope context manager

--- nameko_sqlalchemy/__init__.py
+++ nameko_sqlalchemy/__init__.py
@@ -1,3 +1,4 @@
 from nameko_sqlalchemy.database_session import (  # noqa: F401
     DatabaseSession, DB_URIS_KEY, Session
 )
+from nameko_sqlalchemy.session_scope import DatabaseSessionScope  # noqa: F401
