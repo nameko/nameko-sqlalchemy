b0693de72ded6d8b588bc3797ac704d51ba04723
Ondrej Kohout <ondrej.kohout@gmail.com>
2017-11-20 14:10:26 +0000

    Draft of a Database dependency provider with a session factory and with a lazily managed worker scope session

--- nameko_sqlalchemy/__init__.py
+++ nameko_sqlalchemy/__init__.py
@@ -1,3 +1,1 @@
-from nameko_sqlalchemy.database_session import (  # noqa: F401
-    DatabaseSession, DatabaseSessionScope, DB_URIS_KEY, Session
-)
+from nameko_sqlalchemy.database import Database, DB_URIS_KEY  # noqa: F401
